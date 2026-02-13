394c47b708cfe6f3ed6123ee045b9a5bc2023691
Bhargav51212 <bhargavbandiralla@gmail.com>
2026-02-13 11:01:58 +0530

    Delete Desktop/a directory

--- Desktop/a/backend/routes/paymentRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { verifyPayment } = require('../controllers/paymentController');
-
-// Payment verification route
-router.post('/verify-payment/:orderId', verifyPayment);
-
-module.exports = router; 
